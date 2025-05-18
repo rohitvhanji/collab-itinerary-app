3296a1501458748af62564c64308b615285a2499
rohitvhanji <56556783+rohitvhanji@users.noreply.github.com>
2025-05-18 20:09:06 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
-import './index.css';
+// client/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // Import global styles
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
